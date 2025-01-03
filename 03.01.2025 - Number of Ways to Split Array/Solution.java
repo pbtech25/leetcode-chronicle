c3bf14810fb2a2eb2466cd89a2aa26da2d3e8977
pbtech25 <pbtech25@gmail.com>
2025-01-03 10:24:23 +0530

    LeetCode DCC - 03 JAN 2025

---
+++ 03.01.2025 - Number of Ways to Split Array/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int waysToSplitArray(int[] nums) {
+        int n = nums.length;
+        long totalSum = 0;
+        long prefixSum = 0;
+        int count = 0;
+
+        for (int num : nums) {
+            totalSum += num;
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            prefixSum += nums[i];
+            long suffixSum = totalSum - prefixSum;
+            if (prefixSum >= suffixSum) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
